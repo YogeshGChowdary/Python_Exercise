cee907d239d069c7520486b0968c2d904caf5fa8
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-04 23:45:53 +0530

    added file

---
+++ Excercise-5.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+years_left = 90-age
+days_left = years_left * 365
+months_left = years_left * 12
+weeks_left = years_left * 52
+
+print("you have", days_left, 'days', weeks_left, 'weeks', 'and', months_left, )
+
