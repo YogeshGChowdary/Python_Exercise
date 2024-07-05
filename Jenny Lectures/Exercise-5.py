2406d8325dcb616dc134a0a69111d158a61781d4
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-05 23:19:39 +0530

    added files

--- Jenny Lectures/Exercise-5.py
+++ Jenny Lectures/Exercise-5.py
@@ -1,3 +1,8 @@
+# #How many days, weeks, months we have left to live until 90 years old
+# input = your current age
+# output = you have a days, b weeks and c months left
+# --- 1year = 365days, 1year = 52weeks, 1year = 12months
+
 age = int(input("Enter your age: "))
 
 years_left = 90-age
