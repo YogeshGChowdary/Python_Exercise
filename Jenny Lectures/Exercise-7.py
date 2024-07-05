a1c5f137a03d6272c4ff6c4a3881f774d5a92fb1
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-05 23:17:39 +0530

    git commit -m "added files"

---
+++ Jenny Lectures/Exercise-7.py
@@ -0,0 +1,17 @@
+# BMI = weight/((height)**2)
+
+wt = float(input('enter weight in kgs: '))
+ht = float(input('enter height in mts: '))
+
+bmi = wt/((ht)**2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi} and you are underweight.")
+elif bmi < 25:
+    print('your bmi is', bmi, 'and you are normal weight')
+elif bmi < 30:
+    print("your bmi is", bmi, 'and you are over weight')
+elif bmi < 35:
+    print(f"your bmi is {bmi} and you are obese")
+else:
+    print(f'your bmi is {bmi} and you are clinically obese')
