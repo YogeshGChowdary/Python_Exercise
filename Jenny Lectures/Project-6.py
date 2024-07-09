6f58e9902677e50876b7f1d635a7a4bb2f2f4f1e
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-09 14:07:48 +0530

    added files

---
+++ Jenny Lectures/Project-6.py
@@ -0,0 +1,43 @@
+# Calculator
+
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+operations_dict = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+}
+
+def calculator():
+    number1 = float(input('Enter first number: '))
+    for symbol in operations_dict:
+        print(symbol)
+
+    continue_flag = True
+    while continue_flag:  #while true condition
+        operation_symbol = input("pick an operation: ")
+        number2 = float(input('Enter next number: '))
+
+        calculator_function = operations_dict[operation_symbol]
+        output = calculator_function(number1,number2)
+        print(f'{number1} {operation_symbol} {number2} = {output}')
+
+        should_continue = input(f"Enter 'y' to continue calculation with {output} or 'n' to start a new calculation or 'x' to exit: ").lower()
+        if should_continue == 'y':
+            number1 = output
+        elif should_continue == 'n':
+            continue_flag = False
+            calculator()
+        else:
+            continue_flag = False
+            print("Bye")
+
+calculator()
