74c9823081a7a19d1888dd4d26fb563c2f15635d
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-09 09:26:34 +0530

    added files

--- Jenny Lectures/Exercise-22.py
+++ Jenny Lectures/Exercise-22.py
@@ -1,3 +1,17 @@
 # prime checker
 
+import math
+def prime_checker(num):
+    is_prime = True
+    if num == 1:
+        is_prime = False
+    for i in range (2,math.ceil(num/2)+1): # to check prime, range half of given num is sufficient to check
+        if num%i == 0:
+            is_prime=False
+    if is_prime == True:
+        print('It is a prime number')
+    else:
+        print('Not a prime number')
+
 num = int(input('please enter no. to check:\n'))
+prime_checker(num)
