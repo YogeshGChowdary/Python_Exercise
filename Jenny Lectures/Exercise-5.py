e0fd56d3368dd7c935fc3094217682920b9dc13e
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2024-07-12 22:45:39 +0530

    added files

--- Jenny Lectures/Exercise-5.py
+++ Jenny Lectures/Exercise-5.py
@@ -6,7 +6,7 @@
 age = int(input("Enter your age: "))
 
 years_left = 90-age
-days_left = years_left * 365
+days_left = years_left * 365 
 months_left = years_left * 12
 weeks_left = years_left * 52
 
